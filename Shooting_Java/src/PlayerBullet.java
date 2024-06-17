b77aa57bceee80b60063b567fb7c97be0c0c719f
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-06-17 11:39:23 +0900

    練習問題10-1

--- Shooting_Java/src/PlayerBullet.java
+++ Shooting_Java/src/PlayerBullet.java
@@ -2,5 +2,11 @@
 public class PlayerBullet extends Character {
 	public PlayerBullet(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
+		
+		
+	}
+	public void draw(MyFrame f) {
+		f.setColor(256, 128, 0);
+		f.fillOval(x, y, 30, 30);
 	}
 }
